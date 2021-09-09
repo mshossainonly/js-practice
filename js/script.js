e62bb3875ed4fcdc893e1a642cb5e289415da3b3
Md Sakhawat Hossain <mdsakhawathossainonly@gmail.com>
2021-09-09 15:14:22 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//text change 
+function textChange(){
+	document.getElementById('h2').innerHTML="Welcome to my project";
+}
+function textChange(){
+	document.getElementsByClassName('h2')[1].innerHTML="Welcome to my project";
+}
