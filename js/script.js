574a74108bd5a8124dbeb4c47ad513d09fb49928
Md Sakhawat Hossain <mdsakhawathossainonly@gmail.com>
2021-09-09 20:13:21 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 //text change 
-function textChange(){
-	document.getElementById('h2').innerHTML="Welcome to my project";
+function textChangeText(){
+	document.getElementById('h2').innerHTML = "Welcome to my project";
 }
+
 function textChange(){
-	document.getElementsByClassName('h2')[1].innerHTML="Welcome to my project";
+	document.getElementsByClassName('h3')[2].innerHTML = "Welcome to Shikhbe Sobai.It is an online training center.";
+}
+
+function textChangeHover(){
+	document.getElementById('h4').innerHTML = "Welcome to SSB";
+}
+function textChangeDate(){
+	document.getElementById('date').innerHTML = Date();
+}
+
+function textChangeColor(){
+	document.getElementsByClassName('red').style.color = "red";
+}
+
+function textChangeColor(){
+	document.getElementById('red').style.color = "lime";
+}
+function textChangeSize(){
+	document.getElementById('fsize').style.fontSize = "60px";
+}
+
+function textChangeBorder(){
+	document.getElementById('border').style.border ="5px solid green";
 }
